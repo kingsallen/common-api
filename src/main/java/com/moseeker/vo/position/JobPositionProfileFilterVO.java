6cfc4760797ecd832bd8f7b4bde2918e1d5a591a
yin.yang <yin.yang@centalent.cn>
2018-12-14 17:29:11 +0800

    添加VO

---
+++ src/main/java/com/moseeker/vo/position/JobPositionProfileFilterVO.java
@@ -0,0 +1,15 @@
+package com.moseeker.vo.position;
+
+import lombok.Data;
+
+/**
+ * @Date: 2018/12/14
+ * @Author: JackYang
+ */
+@Data
+public class JobPositionProfileFilterVO {
+
+
+    private Integer pid;
+    private Integer pfid;
+}
