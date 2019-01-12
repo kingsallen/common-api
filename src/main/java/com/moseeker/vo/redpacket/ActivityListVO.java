7a331c8ed1cd8166bac9011533859aaa9efc5a9c
jack frame weng <jack@wjfs-MacBook-Pro.local>
2019-01-12 16:37:14 +0800

    红包列表增加红包上限字段

--- src/main/java/com/moseeker/vo/redpacket/ActivityListVO.java
+++ src/main/java/com/moseeker/vo/redpacket/ActivityListVO.java
@@ -1,5 +1,7 @@
 package com.moseeker.vo.redpacket;
 
+import lombok.Data;
+
 import java.util.List;
 
 /**
@@ -9,6 +11,7 @@ import java.util.List;
  * @Date 2018/12/7 5:34 PM
  * @Version 1.0
  */
+@Data
 public class ActivityListVO {
 
     private Integer id;
@@ -32,6 +35,8 @@ public class ActivityListVO {
     private Double remain;
     private Byte dType;
 
+    private Integer throttle;
+
     private Integer estimatedTotal;
 
     private List<Position> positionList;
@@ -40,192 +45,9 @@ public class ActivityListVO {
         return new Position();
     }
 
+    @Data
     public class Position {
         private int id;
         private String title;
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getType() {
-        return type;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    public Integer getCompanyId() {
-        return companyId;
-    }
-
-    public void setCompanyId(Integer companyId) {
-        this.companyId = companyId;
-    }
-
-    public Integer getTarget() {
-        return target;
-    }
-
-    public void setTarget(Integer target) {
-        this.target = target;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-    public Double getTotalAmount() {
-        return totalAmount;
-    }
-
-    public void setTotalAmount(Double totalAmount) {
-        this.totalAmount = totalAmount;
-    }
-
-    public Double getRangeMin() {
-        return rangeMin;
-    }
-
-    public void setRangeMin(Double rangeMin) {
-        this.rangeMin = rangeMin;
-    }
-
-    public Double getRangeMax() {
-        return rangeMax;
-    }
-
-    public void setRangeMax(Double rangeMax) {
-        this.rangeMax = rangeMax;
-    }
-
-    public Integer getProbability() {
-        return probability;
-    }
-
-    public void setProbability(Integer probability) {
-        this.probability = probability;
-    }
-
-    public String getHeadline() {
-        return headline;
-    }
-
-    public void setHeadline(String headline) {
-        this.headline = headline;
-    }
-
-    public String getHeadlineFailure() {
-        return headlineFailure;
-    }
-
-    public void setHeadlineFailure(String headlineFailure) {
-        this.headlineFailure = headlineFailure;
-    }
-
-    public String getShareTitle() {
-        return shareTitle;
-    }
-
-    public void setShareTitle(String shareTitle) {
-        this.shareTitle = shareTitle;
-    }
-
-    public String getShareDesc() {
-        return shareDesc;
-    }
-
-    public void setShareDesc(String shareDesc) {
-        this.shareDesc = shareDesc;
-    }
-
-    public String getShareImg() {
-        return shareImg;
-    }
-
-    public void setShareImg(String shareImg) {
-        this.shareImg = shareImg;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public Integer getCheck() {
-        return check;
-    }
-
-    public void setCheck(Integer check) {
-        this.check = check;
-    }
-
-    public Double getRemain() {
-        return remain;
-    }
-
-    public void setRemain(Double remain) {
-        this.remain = remain;
-    }
-
-    public List<Position> getPositionList() {
-        return positionList;
-    }
-
-    public void setPositionList(List<Position> positionList) {
-        this.positionList = positionList;
-    }
-
-    public Byte getdType() {
-        return dType;
-    }
-
-    public void setdType(Byte dType) {
-        this.dType = dType;
-    }
-
-    public Integer getEstimatedTotal() {
-        return estimatedTotal;
-    }
-
-    public void setEstimatedTotal(Integer estimatedTotal) {
-        this.estimatedTotal = estimatedTotal;
     }
 }
