abbfa18a64988d1e0913c72bfa0bd62b8de6e4d2
李成飞 <lichengfei@moseeker.com>
2019-01-29 11:25:32 +0800

    猎头管理添加VO

---
+++ src/main/java/com/moseeker/vo/company/basic/HeadhunterLoginVO.java
@@ -0,0 +1,23 @@
+package com.moseeker.vo.company.basic;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * HeadhunterLoginVO
+ *
+ * @Author: lee
+ * @Date: 2019/1/29
+ */
+@Data
+@ApiModel("猎头登录VO")
+public class HeadhunterLoginVO {
+
+    @ApiModelProperty(value = "猎头邮箱")
+    private String email;
+    @ApiModelProperty(value = "登录密码")
+    private String password;
+    @ApiModelProperty(value = "公司ID")
+    private Integer companyId;
+}
