77034d7664c5181cdf2fd9abec333ac3438a42da
zztaiwll <zhangzeteng@moseeker.com>
2019-02-19 09:48:25 +0800

    添加类

--- src/main/java/com/moseeker/vo/position/JobPositionForAppCVDataVO.java
+++ src/main/java/com/moseeker/vo/position/JobPositionForAppCVDataVO.java
@@ -16,8 +16,12 @@ import java.util.List;
  * @DATE 19/2/18 下午3:44
  **/
 @Data
-@ApiModel
+@ApiModel("模板下职位的数据模型")
 public class JobPositionForAppCVDataVO {
+    @ApiModelProperty(name="appCvConfigId",value="自定义模板id",required = true)
     private int appCvConfigId;
+    @ApiModelProperty(name="count",value="职位的数量",required = true)
+    private int count;
+    @ApiModelProperty(name="positions",value="职位信息",required = true)
     private List<JobPositionSimpleDataVO> positions;
 }
