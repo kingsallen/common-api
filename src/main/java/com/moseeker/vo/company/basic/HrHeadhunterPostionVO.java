c03520702d9c830eb5844ad32e335c5b35dfde1d
李成飞 <lichengfei@moseeker.com>
2019-02-26 17:03:01 +0800

    参数调整

--- src/main/java/com/moseeker/vo/company/basic/HrHeadhunterPostionVO.java
+++ src/main/java/com/moseeker/vo/company/basic/HrHeadhunterPostionVO.java
@@ -1,8 +1,11 @@
 package com.moseeker.vo.company.basic;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.sql.Timestamp;
 
 /**
  * HrHeadhunterPostionVO
@@ -22,6 +25,9 @@ public class HrHeadhunterPostionVO {
     private String departmentName;
     @ApiModelProperty(value = "发布人")
     private String hrName;
+    @ApiModelProperty(value = "发布时间", dataType= "string", example = "2018-12-10 10:12:45")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Timestamp syncTime;
     @ApiModelProperty(value = "候选人数")
     private String candidateCount;
 
