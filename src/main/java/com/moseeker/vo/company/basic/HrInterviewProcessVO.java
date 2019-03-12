a6cc34dae0c3fc6ae9ee5f10134fc2acee4f68a2
JackYang <yangtiger0306@163.com>
2019-03-12 12:05:55 +0800

    修该pom版本号

--- src/main/java/com/moseeker/vo/company/basic/HrInterviewProcessVO.java
+++ src/main/java/com/moseeker/vo/company/basic/HrInterviewProcessVO.java
@@ -27,11 +27,16 @@ public class HrInterviewProcessVO {
     private Integer disabled =0;
     @ApiModelProperty(value = "逻辑删除 0 未删除 1已删除")
     private Integer deleted =0;
+    @ApiModelProperty(value = "是否是默认面试流程")
+    private Integer isDefaultProcess=0;
+
     @ApiModelProperty(value = "创建时间", dataType = "java.lang.String", example = "2018-11-28 16:00:00")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Timestamp createTime;
     @ApiModelProperty(value = "更新时间", dataType = "java.lang.String", example = "2018-11-28 16:00:00")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Timestamp updateTime;
+
+
 
 }
