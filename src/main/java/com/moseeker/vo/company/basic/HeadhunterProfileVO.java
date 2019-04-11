0119b1d0cd984576f2d61fd0a046dab7740d5c54
李成飞 <lichengfei@moseeker.com>
2019-04-11 12:57:24 +0800

    猎头简历格式调整

--- src/main/java/com/moseeker/vo/company/basic/HeadhunterProfileVO.java
+++ src/main/java/com/moseeker/vo/company/basic/HeadhunterProfileVO.java
@@ -1,7 +1,6 @@
 package com.moseeker.vo.company.basic;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.moseeker.vo.parsing.ProfileParsingVO;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -26,7 +25,7 @@ public class HeadhunterProfileVO {
     private Integer jobId;
     @ApiModelProperty(value = "简历状态 0 ： 猎头上传， 1 ： hr 接收（正式简历）", dataType="java.lang.Integer")
     private Byte type;
-    private ProfileParsingVO profileParsingVO;
+    private HeadhunterProfileParsingVO profileParsingVO;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @ApiModelProperty(value = "创建时间", dataType = "java.lang.String", example = "2018-12-10 10:12:45")
