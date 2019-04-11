dd917686860834c88b417b2e0e4f974f2edef49d
李成飞 <lichengfei@moseeker.com>
2019-04-11 15:10:35 +0800

    猎头简历上传

--- src/main/java/com/moseeker/vo/company/basic/HeadhunterProfileVO.java
+++ src/main/java/com/moseeker/vo/company/basic/HeadhunterProfileVO.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.sql.Timestamp;
+import java.util.Map;
 
 /**
  * HeadhunterProfileVO
@@ -25,7 +26,7 @@ public class HeadhunterProfileVO {
     private Integer jobId;
     @ApiModelProperty(value = "简历状态 0 ： 猎头上传， 1 ： hr 接收（正式简历）", dataType="java.lang.Integer")
     private Byte type;
-    private HeadhunterProfileParsingVO profileParsingVO;
+    private Map<String,Object> profileParsingVO;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @ApiModelProperty(value = "创建时间", dataType = "java.lang.String", example = "2018-12-10 10:12:45")
