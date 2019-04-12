b7ad76d450e921b95ada5ed22ad49f33bf6af341
李成飞 <lichengfei@moseeker.com>
2019-04-12 19:55:19 +0800

    猎头职位参数调整

--- src/main/java/com/moseeker/vo/company/basic/HeadhunterDataFormVO.java
+++ src/main/java/com/moseeker/vo/company/basic/HeadhunterDataFormVO.java
@@ -18,8 +18,10 @@ public class HeadhunterDataFormVO {
     private Integer headhunterId;
     @ApiModelProperty(value = "职位名称")
     private String positionName;
-    @ApiModelProperty(value = "发布人")
+    @ApiModelProperty(name = "hrId", value = "当前登录hrId")
     private Integer hrId;
+    @ApiModelProperty(name = "publisher", value = "发布人")
+    private Integer publisher;
     @ApiModelProperty(value = "每页条数")
     private Integer pageSize;
     @ApiModelProperty(value = "当前页码")
