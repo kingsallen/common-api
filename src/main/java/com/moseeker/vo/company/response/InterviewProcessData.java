4219a30c7d7b829044cf522a6e721f720c2f60a1
zhangzeteng@moseeker.com <zhangzeteng@moseeker.com>
2019-06-03 17:22:10 +0800

    add

---
+++ src/main/java/com/moseeker/vo/company/response/InterviewProcessData.java
@@ -0,0 +1,9 @@
+package com.moseeker.vo.company.response;
+
+import lombok.Data;
+
+@Data
+public class InterviewProcessData {
+    private int interviewerId;
+    private String interviewRoundName;
+}
