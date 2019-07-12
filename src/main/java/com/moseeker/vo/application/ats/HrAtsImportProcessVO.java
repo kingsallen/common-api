cd3ab983324b5f7f156d5e5e96d9f7e079768d34
李成飞 <lichengfei@moseeker.com>
2019-07-12 15:42:28 +0800

    候选人状态更新接口

---
+++ src/main/java/com/moseeker/vo/application/ats/HrAtsImportProcessVO.java
@@ -0,0 +1,28 @@
+package com.moseeker.vo.application.ats;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * HrAtsImportProcessVO
+ *
+ * @Author: lee
+ * @Date: 2019/7/12
+ */
+@Data
+@ApiModel("候选人流程导入VO")
+public class HrAtsImportProcessVO {
+    @ApiModelProperty(name = "userId", value = "用户Id", dataType = "int", required = true, example = "82752")
+    private Integer userId;
+    @ApiModelProperty(name = "userName", value = "用户姓名", dataType = "string", required = true, example = "张三")
+    private String userName;
+    @ApiModelProperty(name = "mobile", value = "手机号", dataType = "string", required = true, example = "17854282752")
+    private String mobile;
+    @ApiModelProperty(name = "userId", value = "邮箱", dataType = "string", required = true, example = "example@moseeker.com")
+    private String email;
+    @ApiModelProperty(name = "positionId", value = "职位ID", dataType = "int", required = true, example = "52635")
+    private Integer positionId;
+    @ApiModelProperty(name = "userId", value = "//阶段标识（目标阶段", dataType = "int", required = true, example = "666")
+    private Integer processId;
+}
