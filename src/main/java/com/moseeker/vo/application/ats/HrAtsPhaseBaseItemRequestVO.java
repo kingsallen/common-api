61a4e657529bb4bb15f07e099bc20a697531e000
zhangzeteng@moseeker.com <zhangzeteng@moseeker.com>
2019-07-22 11:26:02 +0800

    add

--- src/main/java/com/moseeker/vo/application/ats/HrAtsPhaseBaseItemRequestVO.java
+++ src/main/java/com/moseeker/vo/application/ats/HrAtsPhaseBaseItemRequestVO.java
@@ -9,7 +9,7 @@ import java.util.List;
 @ApiModel("流程配置项的请求参数")
 public class HrAtsPhaseBaseItemRequestVO extends HrAtsPhaseBaseItemVO {
     @ApiModelProperty(name="openList",value = "标志功能开启的列表")
-    private List<OpenEventVO>  openList;
+    private List<Integer>  openList;
     @ApiModelProperty(name="eventList",value = "事件功能列表")
     private List<EventMessageVO> eventList;
     @ApiModelProperty(name="hrId",value = "hr编号")
