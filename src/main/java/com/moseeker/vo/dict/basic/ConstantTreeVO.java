daa195e7d7f2a4ccc96c70adeb85f8679d757d62
jack frame weng <wjf2255@gmail.com>
2019-08-30 19:59:06 +0800

    添加可排序校验

--- src/main/java/com/moseeker/vo/dict/basic/ConstantTreeVO.java
+++ src/main/java/com/moseeker/vo/dict/basic/ConstantTreeVO.java
@@ -9,11 +9,22 @@ import java.util.List;
 
 @Data
 @JsonFilter("emptyfilter")
-public class ConstantTreeVO  extends  BaseFieldValue {
+public class ConstantTreeVO  extends  BaseFieldValue implements Comparable<ConstantTreeVO>{
     private Integer parentCode;
     @JsonIgnore
     private Integer priority;
     @ApiModelProperty("最多一次选择菜单个数")
     private Integer maxSelect;
     private List<ConstantTreeVO> children;
+
+    @Override
+    public int compareTo(ConstantTreeVO o) {
+        if (o == null || o.getPriority() == null) {
+            return -1;
+        }
+        if (this.getPriority() == null) {
+            return 1;
+        }
+        return this.getPriority() - o.getPriority();
+    }
 }
