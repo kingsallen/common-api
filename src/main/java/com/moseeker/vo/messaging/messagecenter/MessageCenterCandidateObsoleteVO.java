e5f83bf6a8488f1b3f832b2ea2cae18c75a3930c
叶虎 <yehu@moseeker.com>
2019-09-07 14:17:20 +0800

    update

---
+++ src/main/java/com/moseeker/vo/messaging/messagecenter/MessageCenterCandidateObsoleteVO.java
@@ -0,0 +1,25 @@
+package com.moseeker.vo.messaging.messagecenter;
+
+import com.moseeker.constant.messaging.MessageCenterNoticeType;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * @author yehu
+ * date 2019-09-07 14:15
+ */
+@Data
+@Builder
+@ApiModel(description = "消息中心-淘汰")
+public class MessageCenterCandidateObsoleteVO implements IMessageCenterBaseVO {
+
+    @ApiModelProperty("淘汰记录id")
+    private Integer hrAtsObsoleteId;
+
+    @Override
+    public MessageCenterNoticeType getMessageCenterNoticeType() {
+        return null;
+    }
+}
