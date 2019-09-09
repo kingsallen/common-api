63caafdca9e4d6452f7a3aa6b4a476f53f76f11a
chenhan <chenhan@moseeker.com>
2019-09-09 10:23:16 +0800

    申请归档

---
+++ src/main/java/com/moseeker/vo/application/ats/ApplicationArchiveParamsVO.java
@@ -0,0 +1,22 @@
+package com.moseeker.vo.application.ats;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class ApplicationArchiveParamsVO {
+    @ApiModelProperty(name = "applicationId", value = "职位id", dataType = "int")
+    private Integer applicationId;
+    @ApiModelProperty(name = "archiveType", value = "归档类型 1:已入职 2:放弃入职 3:淘汰", dataType = "int")
+    private Integer archiveType;
+    @ApiModelProperty(name = "archiveReasonCode", value = "归档原因编号", dataType = "int")
+    private Integer archiveReasonCode;
+    @ApiModelProperty(name = "archiveReason", value = "归档原因")
+    private String archiveReason;
+    @ApiModelProperty(name = "archivePhase", value = "归档阶段", dataType = "int")
+    private Integer archivePhase;
+    @ApiModelProperty(name = "operator", value = "操作人")
+    private String operator;
+}
