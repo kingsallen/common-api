d8ea7708862bd1571d9b3331ced4cfe77e84f4c6
chenhan <chenhan@moseeker.com>
2019-09-12 10:42:17 +0800

    批量归档

--- src/main/java/com/moseeker/vo/application/ats/ApplicationArchiveParamsVO.java
+++ src/main/java/com/moseeker/vo/application/ats/ApplicationArchiveParamsVO.java
@@ -4,13 +4,14 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 public class ApplicationArchiveParamsVO {
     @ApiModelProperty(name = "hrId", value = "hrId", dataType = "int")
     private Integer hrId;
     @ApiModelProperty(name = "applicationId", value = "职位id", dataType = "int")
-    private Integer applicationId;
+    private List<Integer> applicationIds;
     @ApiModelProperty(name = "archiveType", value = "归档类型 1:已入职 2:放弃入职 3:淘汰", dataType = "int")
     private Integer archiveType;
     @ApiModelProperty(name = "archiveReasonCode", value = "归档原因编号", dataType = "int")
