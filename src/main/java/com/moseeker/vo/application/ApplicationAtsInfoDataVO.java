8bfac89e371c6404fa9ca33337eb502c520471c6
zhangxiang <zhangxiang@moseeker.com>
2019-11-25 11:15:30 +0800

    add application vo

---
+++ src/main/java/com/moseeker/vo/application/ApplicationAtsInfoDataVO.java
@@ -0,0 +1,21 @@
+package com.moseeker.vo.application;
+
+import lombok.Data;
+
+@Data
+public class ApplicationAtsInfoDataVO {
+
+    private Integer id;
+
+    private Integer positionId;
+
+    private String jobnumber;
+
+    private Integer applierId;
+
+    private Integer phaseId;
+
+    private Integer processId;
+
+    private Integer atsStatus;
+}
