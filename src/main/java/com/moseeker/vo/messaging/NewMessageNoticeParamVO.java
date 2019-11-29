49dc4ec1b283e59c3a9c262d3b050841dd73c227
xuesong <xuesong@moseeker.com>
2019-11-29 13:44:49 +0800

    commit

---
+++ src/main/java/com/moseeker/vo/messaging/NewMessageNoticeParamVO.java
@@ -0,0 +1,30 @@
+package com.moseeker.vo.messaging;
+
+import lombok.Data;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Data
+@Component
+@Scope("prototype")
+public class NewMessageNoticeParamVO {
+
+    private Integer messageId;
+
+    private String receiverName;
+
+    private String receiverEmail;
+
+    private String receiverMobile;
+
+    private Integer receiverCompanyId;
+
+    private String openId;
+
+    private boolean isQRcode;
+
+    private Map<String,String> originParam;
+
+}
